c2732e409e8490538008629c68e67f8a2d984038
黄照权 <54708329+Only159@users.noreply.github.com>
2024-03-08 00:06:06 +0800

    feat:init DDD project

---
+++ chatbot-api-interface/src/main/java/com/hzq/chatbot/api/ApiApplication.java
@@ -0,0 +1,16 @@
+package com.hzq.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 黄照权
+ * @Date  2024/3/7
+ * @Description 启动入口
+ **/
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class,args);
+    }
+}
---
+++ chatbot-api-interface/src/test/java/com/hzq/chatbot/api/test/ApiTest.java
@@ -0,0 +1,9 @@
+package com.hzq.chatbot.api.test;
+
+/**
+ * @author 黄照权
+ * @Date 2024/3/7
+ * @Description 单元测试
+ **/
+public class ApiTest {
+}
